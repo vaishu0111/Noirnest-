2bd7b5138642dcd4da29f6b9980bafdc0941dfcb
vaishu0111 <vaishalichauhan8266@gmail.com>
2025-07-12 12:25:03 +0530

    Add files via upload

--- Firebase.js
+++ Firebase.js
@@ -1,20 +1,8 @@
-// firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDUMMY_KEY_1234567890",
-  authDomain: "noirnest-app.firebaseapp.com",
-  projectId: "noirnest-app",
-  storageBucket: "noirnest-app.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "XXXXXX",
+  appId: "YOUR_APP_ID"
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
